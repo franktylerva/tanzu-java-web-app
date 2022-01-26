d5b1b8e0bc4f3827c2a511cd1bcce077464da50b
ftyler <ftyler.ctr@kr.af.mil>
2022-01-26 08:35:51 -0500

    Another checkin.

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 	@RequestMapping("/")
 	public String index() {
 		
-		return "Greetings from Spring Boot. Test";
+		return "Greetings from Spring Boot. Test Frank";
 	}
 
 }
